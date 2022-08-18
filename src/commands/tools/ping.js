d588399c0ddfd6503a6115da6ed9a31aeb86ab7c
Maxence <maxence.bellanger@icloud.com>
2022-08-18 23:00:35 +0200

    Enhancement ping cmd

--- src/commands/tools/ping.js
+++ src/commands/tools/ping.js
@@ -1,17 +1,25 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('Pong!'),
+    .setDescription('Effectue un test de latence du bot'),
     async execute(interaction, client) {
         const message = await interaction.deferReply({
             fetchReply: true
         });
 
-        const newMessage = `API Latency: ${client.ws.ping}ms\n Client Ping : ${message.createdTimestamp - message.createdTimestamp}ms`;
-        await interaction.editReply({
-            content: newMessage
-        })
+        const imgBadge = new AttachmentBuilder("./src/database/img/badges/staff-badge.png");
+        const pingEmbed = new EmbedBuilder()
+          .setColor("#838CF6")
+          .setAuthor({ name: `Latence de ${client.user.username}`, iconURL: "attachment://staff-badge.png" })
+          .addFields(
+            { name: "🗄 API Latency", value: `${client.ws.ping}ms`, inline: true },
+            { name: "👥 Client Ping", value: `${message.createdTimestamp - message.createdTimestamp}ms`, inline: true }
+          )
+          .setThumbnail(client.user.displayAvatarURL())
+          .setFooter({ text: client.user.id })
+          .setTimestamp();
+        await interaction.editReply({ embeds: [pingEmbed], files: [imgBadge] });
     }
 }
