91155fd01753e90337a5667f605460e3d117a9ac
Maxence <maxence.bellanger@icloud.com>
2022-08-18 22:07:53 +0200

    Add activity and send msg in logchannel for boot

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,7 +1,28 @@
+const { ActivityType, EmbedBuilder, AttachmentBuilder } = require("discord.js");
+
 module.exports = {
-    name :  "ready",
-    once: true,
-    async execute(client) {
-        console.log(`🟢 ${client.user.tag} is ready!`);
-    },
+  name: "ready",
+  once: true,
+  async execute(client) {
+    console.log(`🟢 ${client.user.tag} is ready!`);
+
+    const membercount = client.user.setPresence({
+      activities: [{ name: `tes commandes ✨`, type: ActivityType.Watching }],
+      status: "online",
+    });
+
+    const logChannel = client.channels.cache.get(process.env.logs);
+    const imgBadge = new AttachmentBuilder("./src/database/img/badges/bot-badge.png");
+    const logEmbed = new EmbedBuilder()
+      .setColor("#9279E2")
+      .setAuthor({ name: "BOT LOGS", iconURL: "attachment://bot-badge.png" })
+      .addFields(
+        { name: "🤖 Client", value: `${client.user}`, inline: true },
+        { name: "🛠 Statut", value: "Boot effectué", inline: true }
+      )
+      .setThumbnail(client.user.displayAvatarURL())
+      .setFooter({ text: client.user.id })
+      .setTimestamp();
+    await logChannel.send({ embeds: [logEmbed], files: [imgBadge] });
+  },
 };
