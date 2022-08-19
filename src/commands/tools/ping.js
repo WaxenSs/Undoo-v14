fe9ab25057bb85c617cb4fa93b348b77d00b40e0
Maxence <maxence.bellanger@icloud.com>
2022-08-19 15:13:50 +0200

    Enhancement ping cmd

--- src/commands/tools/ping.js
+++ src/commands/tools/ping.js
@@ -1,25 +1,25 @@
 const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
+  data: new SlashCommandBuilder()
     .setName('ping-staff')
     .setDescription('Effectue un test de latence du bot'),
     async execute(interaction, client) {
         const message = await interaction.deferReply({
-            fetchReply: true
-        });
+      fetchReply: true,
+    });
 
-        const imgBadge = new AttachmentBuilder("./src/database/img/badges/staff-badge.png");
-        const pingEmbed = new EmbedBuilder()
-          .setColor("#838CF6")
-          .setAuthor({ name: `Latence de ${client.user.username}`, iconURL: "attachment://staff-badge.png" })
-          .addFields(
-            { name: "🗄 API Latency", value: `${client.ws.ping}ms`, inline: true },
-            { name: "👥 Client Ping", value: `${message.createdTimestamp - message.createdTimestamp}ms`, inline: true }
-          )
-          .setThumbnail(client.user.displayAvatarURL())
-          .setFooter({ text: client.user.id })
-          .setTimestamp();
-        await interaction.editReply({ embeds: [pingEmbed], files: [imgBadge] });
-    }
-}
+    const imgBadge = new AttachmentBuilder("./src/database/img/badges/staff-badge.png");
+    const pingEmbed = new EmbedBuilder()
+      .setColor("#838CF6")
+      .setAuthor({ name: `Latence de ${client.user.username}`, iconURL: "attachment://staff-badge.png" })
+      .addFields(
+        { name: "🗄 API Latency", value: `${client.ws.ping}ms`, inline: true },
+        { name: "👥 Client Ping", value: `${message.createdTimestamp - message.createdTimestamp}ms`, inline: true }
+      )
+      .setThumbnail(client.user.displayAvatarURL())
+      .setFooter({ text: client.user.id })
+      .setTimestamp();
+    await interaction.editReply({ embeds: [pingEmbed], files: [imgBadge] });
+  },
+};
